520f63b53a4a4a01657e2972c48d00be9f4a6c6e
Douglas Mencken <dougmencken@gmail.com>
2015-03-08 05:07:43 -0400

    add DontRunMe.info file and directory `rsrc' to git (just nostalgia?)
    exclude resource-fork-only files

---
+++ rsrc/BytecodeMakerVersion.r
@@ -0,0 +1,25 @@
+// ===========================================================================
+// BytecodeMakerVersion.r
+// ===========================================================================
+
+#include "Types.r"
+
+/* Version resource */
+
+// Program
+resource 'vers' (1, "Program") {
+	0x00, 0x60,
+	development, 0x01,
+	verUS,
+	"0.6.0d1",
+	"0.6.0d1 (06/25/03), (c) 1997-2003 Douglas Mencken"
+};
+
+// Suite
+resource 'vers' (2, "Suite") {
+	0x00, 0x60,
+	development, 0x01,
+	verUS,
+	"0.6.0d1",
+	"Bytecode Maker 0.6.0d1"
+};
